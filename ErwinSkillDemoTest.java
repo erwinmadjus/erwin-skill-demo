aa3df04141f9904df2118f0382bf0122f3596ecc
Erwin Madjus <erwinmadjusbusiness@gmail.com>
2022-02-04 18:55:58 -0800

    newagain

--- ErwinSkillDemoTest.java
+++ ErwinSkillDemoTest.java
@@ -5,6 +5,6 @@ import org.junit.*;
 public class ErwinSkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, ErwinSkillDemo.add(1, 3)); // Fails
+        assertEquals(2, ErwinSkillDemo.add(1, 1)); // Fails
     }
 }
